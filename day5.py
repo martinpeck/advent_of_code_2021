52043d26dc0b1dd6a49ee69ae1ae39672c78ec7c
Martin Peck <martinjohnpeck@gmail.com>
2021-12-16 13:09:27 +0000

    generate points of each line

--- day5.py
+++ day5.py
@@ -8,15 +8,38 @@ def parse_data(lines:list):
         m = p.search(line)
         yield [(int(m.group(1)), int(m.group(2))),(int(m.group(3)), int(m.group(4)))]
 
+def generate_line_from_points(point_from, point_to):
+    
+    x1, y1 = point_from
+    x2, y2 = point_to
+    
+    if x1 == x2 or y1 == y2:
+        
+        if x1 > x2:
+            x1, x2 = x2, x1
+
+        if y1 > y2:
+            y1, y2 = y2, y1
+                
+        for x in range(x1, x2 + 1):
+            for y in range(y1, y2 + 1):
+                yield (x, y)
+    
+def place_points_on_board(points):
+    pass
 
 def part1(data: list):
     
     # fill the board
     for item in data:
         from_position = item[0]
-        to_position = item[0]
+        to_position = item[1]
         
-        print(f"fill the board from {from_position} to {to_position}")
+        points = generate_line_from_points(from_position, to_position)
+        
+        board = place_points_on_board(points)
+        
+        print(f"fill the board from {from_position} to {to_position} with line of points {list(points)}")
 
 
 def part2(data: list):
@@ -25,7 +48,7 @@ def part2(data: list):
 
 if __name__ == "__main__":
 
-    with open ("data/day5.txt") as f:
+    with open ("data/day5-test.txt") as f:
         data = parse_data(f.readlines())
 
     print("🦑 🐬 Part1 🐬 🦑")
