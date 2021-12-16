c4bcbd70ae730dad18b9cdcd1184d8af0d5bd8e5
Martin Peck <martinjohnpeck@gmail.com>
2021-12-16 00:42:34 +0000

    refactor song

--- song/days_of_xmas.py
+++ song/days_of_xmas.py
@@ -1,26 +1,26 @@
+gifts = ["a partridge in a pear tree 🌳",
+         "two turtle doves 🕊",
+         "three french hens 🐓",
+         "four calling birds 🦜",
+         "five gold rings 💍",
+         "six geese a laying 🥚",
+         "seven swans a swimming 🦢",
+         "eight maids a milking 🥛",
+         "nine ladies dancing 💃",
+         "ten lords a leaping 🤾",
+         "eleven pipers piping 🎺",
+         "twelve drummers drumming 🥁"]
+
 
 def get_gifts(day):
-    
-    gifts = ["a partridge in a pear tree 🌳", 
-             "two turtle doves 🕊", 
-             "three french hens 🐓", 
-             "four calling birds 🦜", 
-             "five gold rings 💍", 
-             "six geese a laying 🥚", 
-             "seven swans a swimming 🦢",
-             "eight maids a milking 🥛",
-             "nine ladies dancing 💃",
-             "ten lords a leaping 🤾",
-             "eleven pipers piping 🎺",
-             "twelve drummers drumming 🥁"]
-    
     if day == 0:
         return []
 
     return [gifts[day - 1]] + get_gifts(day - 1)
 
+
 def build_gift_message(gift_list):
-    
+
     if len(gift_list) == 1:
         gift_message = gift_list[0]
     else:
@@ -28,11 +28,12 @@ def build_gift_message(gift_list):
         other_items = gift_list[:-1]
         gift_message = "\n\t".join(other_items)
         gift_message += "\n\tand " + last_item[0]
-        
+
     return gift_message
 
+
 def build_day_description(day_number):
-    
+
     if day_number == 1:
         suffix = "st"
     elif day_number == 2:
@@ -41,19 +42,20 @@ def build_day_description(day_number):
         suffix = "rd"
     else:
         suffix = "th"
-        
+
     return f"{day_number}{suffix}"
-        
+
 
 def sing_the_song():
 
-    for current_day in range(1,13):
+    for current_day in range(1, 13):
 
-        gift_list = get_gifts(current_day)     
+        gift_list = get_gifts(current_day)
         gift_message = build_gift_message(gift_list)
         day_description = build_day_description(current_day)
-            
-        print(f"On day {day_description} day of Christmas my true love gave to me:\n\t{gift_message}")
+
+        print(
+            f"On day {day_description} day of Christmas my true love gave to me:\n\t{gift_message}")
         print()
 
 
