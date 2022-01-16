0cc626438d724833ecf22e36ca0e2228909c9f34
Martin Peck <martinjohnpeck@gmail.com>
2022-01-16 14:11:28 +0000

    day 10 part 2

--- day10.py
+++ day10.py
@@ -53,7 +53,47 @@ def part1(data: list):
 
 def part2(data: list, part1_results: list):
     
-    pass               
+    result = None        
+    scores = []
+    
+    for row in data:
+        stack = deque()
+        broken = False
+        for position, instruction in enumerate(row):
+            match instruction:
+                case "[":
+                    stack.appendleft("]")
+                case "<":
+                    stack.appendleft(">")
+                case "{":
+                    stack.appendleft("}")
+                case "(":
+                    stack.appendleft(")")
+                case _:
+                    pair = stack.popleft()
+                    if pair != instruction:
+                        broken = True
+                        continue
+        
+        if not broken:
+            line_score = 0
+            for item in stack:
+                line_score *= 5
+                match item:
+                    case ")":
+                        line_score += 1
+                    case "]":
+                        line_score += 2
+                    case "}":
+                        line_score += 3
+                    case ">":
+                        line_score += 4
+            
+            scores.append(line_score)
+                                    
+    print(f"{sorted(scores)[(len(scores)-1)//2]=}")
+                
+    return result               
 
 if __name__ == "__main__":
 
