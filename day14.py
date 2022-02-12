f233d54d959944da4786c1444558171fbbb9c3d9
Martin Peck <martinjohnpeck@gmail.com>
2022-02-12 17:27:32 +0000

    day 14 part 2

--- day14.py
+++ day14.py
@@ -1,14 +1,13 @@
 from collections import deque, defaultdict, Counter
+from copy import copy
 from typing import Iterator
-
-import plotext as plt
 
 
 def parse_data(data: list[str]):
 
     starting_polymer = ""
     rules = {}
-    
+
     for row_number, row in enumerate([row.strip() for row in data]):
         if row_number == 0:
             starting_polymer = row
@@ -19,49 +18,80 @@ def parse_data(data: list[str]):
     return starting_polymer, rules
 
 
-def pairwise(iterable):    
-    
+def pairwise(iterable):
+
     a : Iterator = iter(iterable)
-    
+
     try:
         first = a.__next__()
-        
-        while True:            
-            second = a.__next__()            
+
+        while True:
+            second = a.__next__()
             yield first + second
             first = second
-        
+
     except StopIteration:
         pass
-            
+
+
+def count_them(iteration, polymer_to_count):
+    c = Counter(polymer_to_count)
+    most_common = c.most_common(1)[0]
+    least_common = c.most_common()[-1]
+    print(f"{iteration=} {most_common=} {least_common=} {most_common[1] - least_common[1]}")
 
 def part1(base_polymer :str, polymer_rules :dict[str, str]):
-    
+
     new_polymer = base_polymer
-    
+
     for i in range(10):
         base_polymer = new_polymer
         new_polymer = ""
         for pair in pairwise(base_polymer):
             element_to_insert = polymer_rules[pair]
-            
             new_polymer += pair[0]
             new_polymer += element_to_insert
-        
+
         new_polymer += pair[1]
-    
-    c = Counter(new_polymer)
-    
-    most_common = c.most_common(1)[0]
-    least_common = c.most_common()[-1]
-    
-    print(f"{most_common=} {least_common=} {most_common[1] - least_common[1]}")
+
+    count_them(i, new_polymer)
 
 
+def part2(base_polymer :str, polymer_rules :dict[str, str]):
 
-def part2(points: list[tuple[int, int]], folds: list):
-    pass
-    
+    first_element = base_polymer[0]
+
+    polymer_pairs = defaultdict(int)
+
+    for pair in pairwise(base_polymer):
+        polymer_pairs[pair] += 1
+
+    for iteration in range(40):
+        pairs = [item for item in polymer_pairs.items() if item[1] > 0]
+
+        for elements, instances in pairs:
+            new_element = polymer_rules[elements]
+
+            new_pair_one = elements[0] + new_element
+            new_pair_two = new_element + elements[1]
+
+            polymer_pairs[new_pair_one] += instances
+            polymer_pairs[new_pair_two] += instances
+            polymer_pairs[elements] -= instances
+
+    totals = Counter()
+
+    totals[first_element] += 1
+
+    for pair_with_count in [item for item in polymer_pairs.items() if item[1] > 0]:
+        # only count the second item in each pair
+        totals[pair_with_count[0][1]] += pair_with_count[1]
+
+    most_common = totals.most_common(1)[0]
+    least_common = totals.most_common()[-1]
+
+    print(f"{iteration=} {most_common=} {least_common=} {most_common[1] - least_common[1]}")
+
 
 if __name__ == "__main__":
 
