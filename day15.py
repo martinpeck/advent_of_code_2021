5fb9e2813b0a96022c6880435cbcd61fd93a25d9
Martin Peck <martinjohnpeck@gmail.com>
2022-02-20 13:19:18 +0000

    day 15 part 1

--- day15.py
+++ day15.py
@@ -1,33 +1,87 @@
+from dataclasses import dataclass
 import random
+from collections import deque, defaultdict
 from sty import bg, ef, fg
 
 
-def print_grid(grid: list[list[str]]):
-    
-    for row in grid:
-        for col in row:
-            
-            highlight = random.random() < .5
-            
+@dataclass
+class Grid:
+    risks: dict
+    rows: int
+    cols: int
+
+
+def print_grid(grid: Grid, costs: dict):
+
+    path = set()
+
+    for r in range(grid.rows):
+        grid_line = ""
+        cost_line = ""
+        for c in range(grid.cols):
+
+            highlight = (r, c) in path
+
             if highlight:
-                print(f"{bg.blue}{ef.b}{col}{ef.rs}{bg.rs}", end='')
+                grid_line += bg.blue + ef.b + str(grid.risks[(r, c)]) + ef.rs + bg.rs
             else:
-                print(f"{fg.da_grey}{col}{fg.rs}", end='')
-        print()
-            
+                grid_line += fg.da_grey + str(grid.risks[(r, c)]) + fg.rs
+
+            cost_line += "{:03d} ".format(costs[r, c])
+
+        print(f"{grid_line}    {cost_line}")
+
 
 def parse_data(data: list[str]) -> list[list[str]]:
 
-    grid = []
-    for row in [row.strip() for row in data]:
-        grid.append([int(col) for col in row])
-        
+    risks = {}
+    for r, row in enumerate([row.strip() for row in data]):
+        for c, col_value in enumerate([int(col) for col in row]):
+            risks[(r, c)] = col_value
+
+    grid = Grid(risks, r + 1, c + 1)
     return grid
 
 
-def part1(grid: list[list[int]]):
-    
-    print_grid(grid)
+def get_neighbours(current_position: tuple[int, int], grid: Grid):
+
+    # north, east, south, west
+    for delta in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+        new_position = (current_position[0] + delta[0], current_position[1] + delta[1])
+        if new_position in grid.risks.keys():
+            yield new_position
+
+
+def part1(grid: Grid):
+
+    visited = set()
+    costs = defaultdict(lambda: -1)
+    costs[(0, 0)] = 0
+
+    frontier = deque()
+    frontier.append((0, 0))
+
+    while len(frontier) > 0:
+        current_position = frontier.popleft()
+        visited.add(current_position)
+        current_position_cost = costs[current_position]
+
+        # get all neighbours
+        for neighbour in get_neighbours(current_position, grid):
+
+            current_neighbour_cost = costs[neighbour]
+            new_neighbour_cost = grid.risks[current_position] + current_position_cost
+
+            if current_neighbour_cost < 0 or (new_neighbour_cost < current_neighbour_cost):
+                costs[neighbour] = new_neighbour_cost
+                if neighbour in visited:
+                    visited.remove(neighbour)
+
+            if neighbour not in visited:
+                visited.add(neighbour)
+                frontier.append(neighbour)
+
+    print_grid(grid, costs)
 
 
 def part2(grid: list[list[int]]):
@@ -36,7 +90,7 @@ def part2(grid: list[list[int]]):
 
 if __name__ == "__main__":
 
-    with open("data/day15-test.txt") as f:
+    with open("data/day15.txt") as f:
         grid = parse_data(f.readlines())
 
     print("🦑 🐬 Part1 🐬 🦑")
